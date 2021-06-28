1ee9231f8f30217ea9550f024855672bd29a08c5
kiet tran <kietbh0704@gmail.com>
2021-06-28 08:46:04 +0700

    t1

---
+++ BigSch/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BigSch.Startup))]
+namespace BigSch
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
