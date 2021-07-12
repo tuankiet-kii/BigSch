1a5d90c1a2d2a64b4efcf69c617db31ab77ad3d3
kiet tran <kietbh0704@gmail.com>
2021-07-12 11:18:34 +0700

    12/7

--- BigSch/Models/ApplicationDbContext.cs
+++ BigSch/Models/ApplicationDbContext.cs
@@ -14,6 +14,8 @@ namespace BigSch.Models
         public DbSet<Category> Categories { get; set; }
         public DbSet<Attendance> Attendances { get; set; }
 
+        public DbSet<Following> Followings { get; set; }
+
         public ApplicationDbContext()
                 : base("DefaultConnection", throwIfV1Schema: false)
             {
@@ -26,6 +28,9 @@ namespace BigSch.Models
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Attendance>().HasRequired(a => a.Course).WithMany().WillCascadeOnDelete(false);
+            modelBuilder.Entity<ApplicationUser>().HasMany(u => u.Followers).WithRequired(f => f.Followee).WillCascadeOnDelete(false);
+            modelBuilder.Entity<ApplicationUser>().HasMany(u => u.Followees).WithRequired(f => f.Follower).WillCascadeOnDelete(false);
+
             base.OnModelCreating(modelBuilder);
         }
     }
